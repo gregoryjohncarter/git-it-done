8f496cd2540fb76a1fab224edf796c3ebbf43198
gregoryjohncarter <gregoryjohncarter@gmail.com>
2021-12-19 15:07:21 -0500

    redirect if invalid repo

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 - App provides functional searches for GitHub usernames and displays their repositories
 - The GitHub API server receives a fetch request and the data gets dynamically added to the page
-
+- When a repository is clicked, the user is brought to a second page with available open issues
--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,19 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+function getRepoName() {
+
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    } else {
+      document.location.replace("./index.html");
+    }
+}
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -18,10 +31,8 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-      console.log(response);
-      alert("There was a problem with your request!");
+    } else {
+      document.location.replace("./index.html");
     }
   });
 };
@@ -81,4 +92,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
